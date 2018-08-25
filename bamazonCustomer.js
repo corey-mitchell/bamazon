0397884ab1e9a88067f0d83553ab94048d74b2c1
corey-mitchell <corey69_96@yahoo.com>
2018-08-25 14:59:41 -0500

    homework ready

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,15 +1,19 @@
+// Dependencies
+require("dotenv").config();
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+
 // Finally found a simple functioning table logger. The documentation requested that it be initalized up here,
 // but I use 'console.table()' to envoke it instead of calling on the variable. Weird, but it works.
-const table = require("console.table");
+require("console.table");
+
 
 // Creates connection info for database
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3308,
     user: "root",
-    password: "password",
+    password: process.env.PW,
     database: "bamazon"
 });
 
@@ -22,6 +26,18 @@ connection.connect((err) => {
     setTimeout(buySomething, 500);
 });
 
+// Makes sure that the user is supplying only positive integers for their inputs
+function validateInput(value) {
+	const integer = Number.isInteger(parseFloat(value));
+	const sign = Math.sign(value);
+
+	if (integer && (sign === 1)) {
+		return true;
+	} else {
+		return 'Please enter a whole non-zero number.';
+	};
+};
+
 // Logs out available products in a table
 const availItems = () => {
     connection.query("SELECT id, product, price FROM products", (err, res)=>{
@@ -30,41 +46,60 @@ const availItems = () => {
     });
 };
 
-// Start function
+// Main function for prompt and handling input
 const buySomething = () => {
     inquirer
         .prompt([{
             name: "itemID",
             type: "input",
-            message: "Please type the ID number of the product you would like to buy."
+            message: "Please type the ID number of the product you would like to buy.",
+            validate: validateInput,
+            filter: Number
         }
         ,
         {
             name: "quantity",
             type: "input",
-            message: "How many units would you like to buy?"
+            message: "How many do you need?",
+            validate: validateInput,
+            filter: Number
         }])
         .then((answer)=>{
-            if(answer.itemID)
-            console.log(`User would like to purchase ${answer.quantity} copies of item ID ${answer.itemID}`);
+            const item = answer.itemID;
+            const quantity = answer.quantity;
+            const queryStr = 'SELECT * FROM products WHERE ?';
 
-
+            // Opens connection to DB
             connection.query(
-                `UPDATE products SET stock=stock-${answer.quantity} WHERE id='${answer.itemID}'`,
+                queryStr, {id: item},
                 (err, res) => {
                     if (err) throw err;
                     
-                    if (res.message[15] === '0') {
+                    // Checks that the user input is a valid item in stock
+                    if (res.length === 0) {
                         console.log("ERROR: Please enter valid ID number!");
                         buySomething();
                     } else {
-                        if(res.stock < answer.quantity) {
-                            console.log("Not enough in stock.")
+                        // Checks stock to so if there is enough. If there isn't then it prompt user for new input
+                        const productData = res[0];
+                        if(quantity > productData.stock) {
+                            console.log("Sorry, we don't have enough in stock.");
+                            buySomething();
                         } else {
-                            console.log(`${res.affectedRows} products updated!`)
+                            // If there is enough stock, this will output the total price of order
+                            console.log(`Your order has been placed. Your total is ${(productData.price * quantity).toFixed(2)}`);
+
+                            // Updates DB to reflect changes.
+                            connection.query(
+                                `UPDATE products SET stock = ${productData.stock - quantity} WHERE id = ${item}`,
+                                (err, res) => {
+                                    if (err) throw err;
+                                }
+                            );
+                            // Closes connection
                             connection.end();
                         };
-                    }
+                    };
                 }
             );
         });
